bd645e5dbd80635559dc5931d179380343e43387
phurit duangmanee <phurit.dua@ntb.co.th>
2021-07-23 15:00:48 +0700

    - add testcase

---
+++ src/main/java/MyRange.java
@@ -0,0 +1,30 @@
+public class MyRange {
+    public static final int ASCII_VALUE = 48;
+    private final String input;
+
+    public MyRange(String input) {
+        this.input = input;
+    }
+
+    public boolean isStartWithInclude() {
+        return this.input.startsWith("[");
+    }
+
+    public int getStart() {
+        if(isStartWithInclude()) {
+            return this.input.charAt(1) - ASCII_VALUE;
+        }
+        return -1;
+    }
+
+    public int getEnd() {
+        if(isEndWithInclude()) {
+            return this.input.charAt(3) - ASCII_VALUE;
+        }
+        return 5;
+    }
+
+    public boolean isEndWithInclude() {
+        return this.input.endsWith("]");
+    }
+}
