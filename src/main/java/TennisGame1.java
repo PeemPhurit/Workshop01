c684ffd34832ee5a5b94f753fc1867aa1fc2df0d
phurit duangmanee <phurit.dua@ntb.co.th>
2021-07-23 18:55:32 +0700

    - Use Boolean

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -3,6 +3,7 @@ import java.util.Objects;
 public class TennisGame1 {
     private int m_score1 = 0;
     private int m_score2 = 0;
+    private int minusTimeScore = 4;
     private final String player1Name;
     private final String player2Name;
 
@@ -21,31 +22,36 @@ public class TennisGame1 {
 
     public String getScore() {
         String[] scoreResults = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
-        int winSet = 4;
-        // All
         if (isAll()) {
             return scoreResults[m_score1] + "-" + "All";
         }
 
-        // Deuce
         if (isDeuce()) {
-            // Deuce
             return "Deuce";
         }
 
-        // ???? Advantage and Win ????
-        if (m_score1 >= winSet || m_score2 >= winSet) {
-            StringBuilder score = new StringBuilder();
-            int minusResult = m_score1 - m_score2;
-            if (minusResult == 1) score = new StringBuilder("Advantage ").append(this.player1Name);
-            else if (minusResult == -1) score = new StringBuilder("Advantage ").append(this.player2Name);
-            else if (minusResult >= 2) score = new StringBuilder("Win for ").append(this.player1Name);
-            else score = new StringBuilder("Win for ").append(this.player2Name);
-            return score.toString();
+        if (isMinus()) {
+            int minusResult = getMinusResult();
+            if (isAdvantage(minusResult)) return "Advantage %s".formatted(getAnnounceNamePlayer(minusResult));
+            else return "Win for %s".formatted(getAnnounceNamePlayer(minusResult));
         }
-
-        // Normal game
         return scoreResults[m_score1] + "-" + scoreResults[m_score2];
+    }
+
+    private boolean isAdvantage(int minusResult) {
+        return Math.abs(minusResult) == 1;
+    }
+
+    private String getAnnounceNamePlayer(int minusResult) {
+        return minusResult <= 0 ? this.player2Name : this.player1Name;
+    }
+
+    private int getMinusResult() {
+        return m_score1 - m_score2;
+    }
+
+    private boolean isMinus() {
+        return m_score1 >= minusTimeScore || m_score2 >= minusTimeScore;
     }
 
     private boolean isDeuce() {
